ed0bfdaa7e10f08f27daf69983225358cf912b7d
Ankit Chaturvedy <chaturvedyankit@gmail.com>
2025-09-05 14:56:21 +0530

    Add files via upload

--- browser.d.ts
+++ browser.d.ts
@@ -1,5 +1,10 @@
-declare const saslprep: (input: string, opts?: {
-    allowUnassigned?: boolean;
-} | undefined) => string;
-export = saslprep;
-//# sourceMappingURL=browser.d.ts.map
+export declare const FormData: {
+    new (form?: HTMLFormElement | undefined): FormData;
+    prototype: FormData;
+}, Blob: {
+    new (blobParts?: BlobPart[] | undefined, options?: BlobPropertyBag | undefined): Blob;
+    prototype: Blob;
+}, File: {
+    new (fileBits: BlobPart[], fileName: string, options?: FilePropertyBag | undefined): File;
+    prototype: File;
+};
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-/**
- * Detect Electron renderer process, which is node, but we should
- * treat as a browser.
- */
-
-if (typeof process !== 'undefined' && process.type === 'renderer') {
-  module.exports = require('./browser.js');
-} else {
-  module.exports = require('./node.js');
-}
+export * from "./FormDataEncoder.js";
+export * from "./FileLike.js";
+export * from "./FormDataLike.js";
+export * from "./util/isFileLike.js";
+export * from "./util/isFormData.js";
